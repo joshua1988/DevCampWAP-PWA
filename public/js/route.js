9a6b363dc8a51c2de9ea55998da752c1ec95ca98
joshua1988 <jangkeehyo@gmail.com>
2017-05-01 10:50:30 +0900

    Added the programmatic routing feature to navigate to Main page after Google auth login

--- public/js/route.js
+++ public/js/route.js
@@ -1,3 +1,5 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
 var Login = { template:
   `<div class="row">
     <div class="input-field col s12">
@@ -15,13 +17,49 @@ var Login = { template:
   </div>`,
   methods: {
     loginWithGoogle: function (event) {
-      alert("hi");
-    }
-  },
-};
-// v-on:click="loginWithGoogle"
+      return firebase.auth().signInWithPopup(provider).then(function(result) {
+       // This gives you a Google Access Token. You can use it to access the Google API.
+       var token = result.credential.accessToken;
+       // The signed-in user info.
+       var user = result.user;
 
-var Main = { template: '<div>bar</div>' };
+       console.log(result);
+       router.push({ path : 'main'});
+      }).catch(function(error) {
+       // Handle Errors here.
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       // The email of the user's account used.
+       var email = error.email;
+       // The firebase.auth.AuthCredential type that was used.
+       var credential = error.credential;
+
+       console.log(error);
+       // ...
+      });
+    }
+  }};
+
+var Main = { template: `
+  <div class="row">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>`,
+  methods: {
+
+  }};
+
 var routes = [
   { path: '/', component: Login },
   { path: '/main', component: Main }
