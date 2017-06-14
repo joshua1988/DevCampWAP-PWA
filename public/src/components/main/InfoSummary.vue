d6efa93e60563f1bea9fb13f6f8c72340d4a2242
joshua1988 <jangkeehyo@gmail.com>
2017-06-14 13:52:56 +0900

    Updated the markup and css in InfoSummary component

--- public/src/components/main/InfoSummary.vue
+++ public/src/components/main/InfoSummary.vue
@@ -1,17 +1,23 @@
 <template>
+  <!-- s-w-info : section-weather-info -->
   <section class="s-w-info">
     <!-- d-w-info : div-weather-info -->
     <div class="d-w-info">
+      <!-- sp-w-elements : span-weather-elements -->
+      <span class="sp-w-elements">미세먼지</span>
       <md-icon class="md-primary">sentiment_very_satisfied</md-icon>
-      <span>미세먼지 : </span>
+      <!-- sp-w-figures : span-weather-figures -->
+      <span class="sp-w-figures">좋음</span>
     </div>
     <div class="d-w-info">
+      <span class="sp-w-elements">이산화질소</span>
       <md-icon class="md-accent">sentiment_neutral</md-icon>
-      <span>이산화질소 : </span>
+      <span class="sp-w-figures">나쁨</span>
     </div>
     <div class="d-w-info">
+      <span class="sp-w-elements">오존</span>
       <md-icon class="md-warn">sentiment_very_dissatisfied</md-icon>
-      <span>오존 : </span>
+      <span class="sp-w-figures">보통</span>
     </div>
   </section>
 </template>
@@ -26,9 +32,16 @@ export default {
   .s-w-info {
     display: flex;
     text-align: center;
+    padding: 2% 0 2% 0;
   }
   .d-w-info {
     width: 33%;
     display: inline-grid;
+  }
+  .sp-w-elements {
+    padding-bottom: 2%;
+  }
+  .sp-w-figures {
+    padding-top: 2%;
   }
 </style>
