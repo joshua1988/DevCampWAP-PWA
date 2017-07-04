f0485f21b87a2f49f182064157392d8d6db1c9d9
joshua1988 <jangkeehyo@gmail.com>
2017-07-04 16:37:34 +0900

    Modified the name of Share component to Statistic

---
+++ public/src/components/main/Statistic.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div>
+    <BarChart></BarChart>
+  </div>
+</template>
+
+<script>
+// https://vuejsfeed.com/blog/create-charts-with-vue-chartjs
+import BarChart from '../../utils/charts.js';
+
+export default {
+  methods: {
+
+  },
+  components: {
+    BarChart
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
