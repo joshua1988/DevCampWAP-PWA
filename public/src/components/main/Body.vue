d26c54bf8ce223821a54b47fc03649f8c7edc1b4
joshua1988 <jangkeehyo@gmail.com>
2017-07-04 09:52:00 +0900

    Added the new open API to display the right weather information from openAPI.seoul.go.kr

--- public/src/components/main/Body.vue
+++ public/src/components/main/Body.vue
@@ -43,8 +43,19 @@ export default {
       var self = this;
 
       // 한글 URL 로 HTTP GET 요청시 발생하는 인코딩 문제 해결 필요
-      var decodedURL = districtName;
-      var url = 'http://openapi.seoul.go.kr:8088/746a5361636a6f7337336e4f656579/json/RealtimeCityAir/1/25/';
+      // var decodedURL = districtName;
+      // var url = 'http://openapi.seoul.go.kr:8088/746a5361636a6f7337336e4f656579/json/RealtimeCityAir/1/25/';
+
+      // 권역 선택 + 자치구 선택
+      // http://openAPI.seoul.go.kr:8088/746a5361636a6f7337336e4f656579/json/RealtimeCityAir/1/5/동북권/성북구
+
+      // 자치구 선택
+      // 대기오염 정보 Open API 활용가이드 - 17페이지
+      var serviceKey = 'TNXrkDjzIjFlOWrBnFo8Q26S5jU%2BN2gWAalGbVsm4QAwIZRzpJtPGSY8YapNMK8RpUahVb2oyycFSWgsDwaxsA%3D%3D';
+      var url = 'https://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=' + districtName + '&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=' + serviceKey + '&ver=1.3';
+      // var url = 'https://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=강남구&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=TNXrkDjzIjFlOWrBnFo8Q26S5jU%2BN2gWAalGbVsm4QAwIZRzpJtPGSY8YapNMK8RpUahVb2oyycFSWgsDwaxsA%3D%3D&ver=1.3';
+      // http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=daily&pageNo=1&numOfRows=1&ServiceKey=TNXrkDjzIjFlOWrBnFo8Q26S5jU%2BN2gWAalGbVsm4QAwIZRzpJtPGSY8YapNMK8RpUahVb2oyycFSWgsDwaxsA%3D%3D&ver=1.3
+
       console.log(url);
 
       return this.$http.get(url).then(function (result) {
